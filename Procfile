d6a62e058b4a768086108f4cc28209c67d330daf
ash160 <ashish160kumar@gmail.com>
2021-08-18 15:25:14 +0530

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn oceannews.wsgi --log-file -
---
+++ news/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include
+from news import views
+from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+]
