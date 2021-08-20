4e145a59a90bfc609f234a2063feec280911e049
ashish2020kashyap <ashish2020kashyap@gmail.com>
2021-08-20 09:10:20 +0530

    settings updated

--- oceannews/settings.py
+++ oceannews/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'oceannews.wsgi.application'
 
+CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:3000',
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
